7bede3185c9e44ffb64f6fdfe7304833f10e0e8d
Karol <Karol.Wo@gmail.com>
2022-08-15 11:44:53 +0200

    replace @NotNull and @Size attributes of pizza.name to @NotEmpty and @Size of pizza.ingredients to @NotEmpty

--- src/main/java/pizza/Pizza.java
+++ src/main/java/pizza/Pizza.java
@@ -3,6 +3,7 @@ package pizza;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
@@ -25,11 +26,10 @@ public class Pizza {
 
     private Date createdAt = new Date();
 
-    @NotNull
-    @Size(min = 1, message = "Name is required")
+    @NotEmpty(message = "Name is required")
     private String name;
 
-    @Size(min = 1, message = "You need to choose at least 1 ingredient")
+    @NotEmpty(message = "You need to choose at least 1 ingredient")
     @ManyToMany()
     private List<Ingredient> ingredients = new ArrayList<>();
 
